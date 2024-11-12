473e1e170bcab9fe8ea94a0eff1a483080d3ab7e
RWParsons <rex.parsons94@gmail.com>
2024-11-12 15:28:30 +1000

    add feature to read dataset(s) by its id

---
+++ R/find-datasets.R
@@ -0,0 +1,60 @@
+#' Find the available measures within a measure category.
+#'
+#' @param measure_category_code A measure category code. See
+#' `get_measure_categories()` for a list.
+#' @param trim Whether or not to exclude the potentially unecessary variables.
+#'
+#' @return data
+#' @export
+#'
+#' @examples
+#' get_measures_from_category("MYH-CANCER")
+get_measures_from_category <- function(measure_category_code, trim = TRUE) {
+  url <- as.character(glue::glue("measure-categories/{measure_category_code}/measures"))
+  d <- call_aihw_api(url)$result |>
+    tidy_resp_to_df()
+
+  if(trim) {
+    d <- d |>
+      dplyr::select(measure_code, measure_name, units = units_units_name)
+  }
+
+  d
+}
+
+
+#' Read (multiple) datasets by id
+#'
+#' @param ids id of the dataset. See `get_datasets()`.
+#' @param return_caveats A logical. Whether or not to return the caveats for
+#' this data in a separate table. If `TRUE`, this will return a list with the
+#' two tables.
+#'
+#' @return data
+#' @export
+#'
+#' @examples
+#' read_dataset_ids(c(1,2))
+read_dataset_ids <- function(ids, return_caveats = FALSE) {
+  d_datasets <- get_datasets() |>
+    dplyr::filter(data_set_id %in% ids)
+
+  if(any(!ids %in% d_datasets$data_set_id)) {
+    missing_ids <- ids[!ids %in% d_datasets$data_set_id] |>
+      paste0(collapse = ", ")
+    warning(glue::glue("IDs passed were not valid (available in `get_datasets()`): {missing_ids}"))
+  }
+
+  dframes <- d_datasets$data_set_id |>
+    purrr::map(read_dataset_by_id) |>
+    dplyr::bind_rows() |>
+    tidy_flat_data_extract(return_caveats = return_caveats)
+
+  dplyr::left_join(d_datasets, dframes)
+}
+
+
+read_dataset_by_id <- function(id) {
+  url <- as.character(glue::glue("datasets/{id}/data-items"))
+  call_aihw_api(url)$result |> tidy_resp_to_df()
+}
---
+++ R/read-flat-data-extract.R
@@ -0,0 +1,101 @@
+#' Get data by measure category.
+#'
+#' @param measure_category_code A measure category code. See
+#' `get_measure_categories()` for valid codes.
+#' @param measure_code A measure code.
+#' @param return_caveats A logical. Whether or not to return the caveats for
+#' this data in a separate table. If `TRUE`, this will return a list with the
+#' two tables.
+#'
+#' @return data
+#' @export
+#'
+#' @examples
+#' \donttest{
+#' read_flat_data_extract(measure_category_code = "MYH-CANCER")
+#' read_flat_data_extract(measure_category_code = "MYH-CANCER", measure_code = "MYH0001")
+#' }
+read_flat_data_extract <- function(measure_category_code, measure_code, return_caveats = FALSE) {
+  assertthat::assert_that(assertthat::is.string(measure_category_code))
+  assertthat::assert_that(measure_category_code %in% get_measure_categories()$measure_category_code)
+
+  measure_code_str <- ""
+  if (!missing(measure_code)) {
+    assertthat::assert_that(assertthat::is.string(measure_code))
+    assertthat::assert_that(measure_code %in% get_measures_from_category(measure_category_code)$measure_code)
+
+    measure_code_str <- paste0("&measure_code=", measure_code)
+  }
+
+  max_rows <- 1000
+
+  res <- call_aihw_api(paste0("flat-data-extract/", measure_category_code, "?skip=0&top=5", measure_code_str))
+
+  total_rows <- res$result$pagination$total_results_available
+
+  skips <- seq(from = 0, to = total_rows - 1, by = max_rows)
+  tops <- purrr::map(skips, ~ min(c(max_rows, total_rows - .x)))
+
+  dframes <- purrr::map2(
+    .x = skips, .y = tops,
+    .f = ~ call_flat_data_segment(
+      url = paste0("flat-data-extract/", measure_category_code),
+      measure_code_str = measure_code_str,
+      skip = .x,
+      top = .y
+    )
+  )
+
+  dframes |>
+    dplyr::bind_rows() |>
+    tidy_flat_data_extract(return_caveats = return_caveats)
+}
+
+tidy_resp_to_df <- function(result) {
+  result |>
+    purrr::map(unlist) |>
+    dplyr::bind_rows() |>
+    janitor::clean_names()
+}
+
+call_flat_data_segment <- function(url, skip, top, measure_code_str) {
+  res <- call_aihw_api(as.character(glue::glue("{url}?skip={skip}&top={top}{measure_code_str}")))
+  tidy_resp_to_df(res$result$data)
+}
+
+tidy_flat_data_extract <- function(data, return_caveats) {
+  if (any(str_detect(names(data), "caveat"))) {
+    if("caveat" %in% names(data)) {
+      d_caveats <- data |>
+        dplyr::select(caveat:dplyr::last_col()) |>
+        dplyr::filter(!is.na(caveat))
+
+      data <- data |>
+        dplyr::select(1:caveat) |>
+        dplyr::filter(is.na(caveat)) |>
+        dplyr::select(-caveat)
+    } else {
+      d_caveats <- data |>
+        dplyr::select(
+          dplyr::starts_with("caveat"),
+          dplyr::starts_with("suppressions")
+        ) |>
+        dplyr::distinct()
+
+      d_caveats <- d_caveats[!is.na(d_caveats[[1]]),]
+
+      data <- data |>
+        dplyr::select(-dplyr::all_of(names(d_caveats)[-1])) |>
+        dplyr::filter(is.na(!!rlang::sym(names(d_caveats)[1]))) |>
+        dplyr::select(-dplyr::all_of(names(d_caveats)[1]))
+    }
+  }
+
+  if (return_caveats) {
+    res <- list(data = data, d_caveats = d_caveats)
+  } else {
+    res <- data
+  }
+
+  res
+}
